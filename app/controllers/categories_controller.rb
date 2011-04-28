fcd26d7d8358da2126f3c9b5bc17b1b164688460
vagmi <vagmi.mudumbai@gmail.com>
2011-04-28 22:28:48 +0530

    cucumber tests for new now work

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,17 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+  def new
+    @categories = Category.all
+    @category = Category.new
+  end
+  def create
+    @category = Category.new(params[:category])
+    if(@category.save)
+      flash[:notice] = "Category saved successfully"
+      redirect_to root_path
+    else
+      render :action=>:new
+    end
+  end
 end
