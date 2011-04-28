bc374f4146c9c853bd78f930a302e1b7e584634e
vagmi <vagmi.mudumbai@gmail.com>
2011-04-28 21:17:52 +0530

    plain bare rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cart::Application.config.secret_token = '5cb0b94f71cbece7a9ebe9d1cdd46958b3fd5a365b4b70f63c0c245e93d94b0b84647361024b0e315a77abd0cbb691ba1e87097de2ae7fba358446972409f7a5'
