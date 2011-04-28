e98dd5f81503a83056288495a4bcb3597eb00542
vagmi <vagmi.mudumbai@gmail.com>
2011-04-28 22:17:53 +0530

    add the new category feature and relevant steps

--- features/categories.feature
+++ features/categories.feature
@@ -13,3 +13,10 @@ Feature: Manage categories
     Then I should see "book"
     And I should see "audio"
     And I should see "electronics"
+
+  Scenario: Add a category
+    Given I am on the home page
+    And I click on the "New category" link
+    And I fill in "books" for "Name"
+    And I click on "Create Category"
+    Then I should see "books" in the list of categories
--- features/step_definitions/category_steps.rb
+++ features/step_definitions/category_steps.rb
@@ -13,3 +13,23 @@ Then /^I should see "([^"]*)"$/ do |name|
   page.should have_content(name)
 end
 
+Given /^I am on the home page$/ do
+  visit root_path
+end
+
+Given /^I click on the "([^"]*)" link$/ do |link_name|
+  click_link(link_name)
+end
+
+Given /^I fill in "([^"]*)" for "([^"]*)"$/ do |value, name|
+  fill_in(name,:with=>value)
+end
+
+Given /^I click on "([^"]*)"$/ do |button_name|
+  click_button(button_name)
+end
+
+Then /^I should see "([^"]*)" in the list of categories$/ do |category_name|
+  page.should have_content(category_name)
+end
+
